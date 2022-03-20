02f301e6e068b7dc85f27bafbfd9d13f76278025
eigmax <stephen@ieigen.com>
2022-03-21 00:17:24 +0800

    Fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Upgrade Contract
+```
+yarn test
+```
 
+# References
+1. https://solidity-by-example.org/app/upgradeable-proxy/
+2. https://jeiwan.net/posts/upgradeable-proxy-from-scratch/
+3. https://dev.to/yakult/tutorial-write-upgradeable-smart-contract-proxy-contract-with-openzeppelin-1916
--- test/5.proxy.test.ts
+++ test/5.proxy.test.ts
@@ -19,25 +19,29 @@ describe("Proxy", function () {
     await v1.deployed()
     await pro.setImplementation(v1.address)
 
-    const abi = ["function initialize() public"];
+    const abi = ["function initialize() public", "function inc() public", "function x() public view returns(uint)"];
     const proxied = new ethers.Contract(pro.address, abi, provider.getSigner(0));
     await proxied.initialize();
-
-    const V2 = await ethers.getContractFactory("Proxy")
-    v2 = await V2.deploy()
-    await v2.deployed()
+    await proxied.inc()
+    expect(await proxied.x()).to.equal(BigNumber.from('1'))
   })
 
   it("should retrieve value previously stored", async function () {
-    await v1.inc()
-    expect(await v1.x()).to.equal(BigNumber.from('1'))
+    const V2 = await ethers.getContractFactory("V2")
+    v2 = await V2.deploy()
+    await v2.deployed()
 
     await pro.setImplementation(v2.address)
-    const abi = ["function inc() public", "function dec() public", "function x() public returns(uint)"];
+    const abi = ["function inc() public", "function dec() public", "function x() public view returns(uint)"];
     const proxied = new ethers.Contract(pro.address, abi, provider.getSigner(0));
 
     expect(await proxied.x()).to.equal(BigNumber.from('1'))
-    proxied.dec()
+    await proxied.dec()
     expect(await proxied.x()).to.equal(BigNumber.from('0'))
+    for (let i = 1; i < 10; i ++) {
+        await proxied.inc()
+        expect(await proxied.x()).to.equal(BigNumber.from(i))
+    }
+    console.log("Final x = ", await proxied.x())
   })
 })
